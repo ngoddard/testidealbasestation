16096d28c1a46dd53cd0153de35bfbde7a11e3a7
Janek Mann <janekm@gmail.com>
2014-07-24 11:23:58 +0100

    Cleanup

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var request = require('request-json');
 var async = require('async');
-var logentries = require('node-logentries');
-//var Proto  = require("node-protobuf").Protobuf;
-//var request = require("request");
-
-//var IdealPB2 = new Proto(fs.readFileSync("ideal.desc"));
 
 var loggly = require('loggly');
- 
+var Enum = require('enum');
+
+var packetType = new Enum({'TEMPHUM': 1,
+                           'BROADCAST': 2,
+                           'CURRENT': 3,
+                           'CLAMPS': 4,
+                           'LIGHT': 5,
+                           'GAS': 6 });
+
  var client = loggly.createClient({
     token: "938edd34-64c3-4da1-9635-275eb194beb4",
     subdomain: "ideallog",
@@ -15,13 +18,10 @@ var loggly = require('loggly');
     json:true
 });
 
-client.log("Hello World from Node.js!");
-
 const HomeOffset = 0;
 const BaseStationAddress = process.env.RESIN_DEVICE_UUID;
 
-
-var jsonClient = request.newClient('http://129.215.164.145:3000/');
+var IDEALJSONClient = request.newClient(process.env.IDEAL_SERVER);
 
 var serialport = require("serialport")
 var SerialPort = serialport.SerialPort
@@ -30,164 +30,76 @@ var serialPort = new SerialPort(process.env.IDEAL_SERIAL, {
   parser: serialport.parsers.readline("\n")
 });
 
-
-var log = logentries.logger({
-  token:process.env.LOGENTRIES_TOKEN
-// token:'cc436528-4be5-4bc5-8230-4835e6268dd3'  
-});
-
-log.info('App starting');
-
-
-setInterval(function(){
-  //log.info('alive2');
-}, 4000);
-
-process.on('uncaughtException', function(err) {
-    // handle the error safely
-    log.err('uncaughtException: ' + err);
-    var killtimer = setTimeout(function() {
-          process.exit(1);
-        }, 3000);
-    // But don't keep the process open just for that!
-    killtimer.unref();
-});
-
-function sendProtobuf(data) {
-  //log.info(data)
-}
-
 function sendJSON(data) {
-  // console.log(data);
-  jsonClient.post('jsonreading/', data, function (err, res, body) {
+  IDEALJSONClient.post('jsonreading/', data, function (err, res, body) {
       if(err) {
-        console.log(err);
+        //console.log(err);
         client.log({"statusCode": res.statusCode});
       } else {
-      //log.info(err);
-      //log.info(res);
-      //log.info(body);
         return client.log({"statusCode": res.statusCode});
       }
   });
 }
 
 serialPort.on("open", function () {
-  log.info('open');
   serialPort.on('data', function(data) {
-    //log.info('data received: ' + data);
+    //client.log('data received: ' + data);
     try {
       js_data = JSON.parse(data);
     } catch (er) {
       return;
     }
-    logly_data = {
+    JSON_data = {
       "basestation_address": BaseStationAddress,
       "sensorbox_address": js_data.node_id + HomeOffset,
       "timestamp": (Date.now())/100,
       "timeinterval": 60
     }
-    out_data = {
-      "basestation_address": BaseStationAddress,
-      "data_samples": [
-        {
-          "sensorbox_address": js_data.node_id + HomeOffset,
-          "timestamp": (Date.now() - 1262304000000)/100,
-          "timeinterval": 60
-        }
-      ]
-    }
     switch(js_data.packet_type) {
-      case 1:
-        out_data["data_samples"][0]["internal_temperature"] = js_data.val0;
-        out_data["data_samples"][0]["humidity"] = js_data.val1;
-        logly_data["internal_temperature"] = js_data.val0;
-        logly_data["humidity"] = js_data.val1;
-        client.log(logly_data);
-        sendJSON(logly_data);
-        sendProtobuf(out_data);
+      case packetType.TEMPHUM:
+        JSON_data["internal_temperature"] = js_data.val0;
+        JSON_data["humidity"] = js_data.val1;
+        client.log(JSON_data);
+        sendJSON(JSON_data);
         break;
-      case 2:
+      case packetType.BROADCAST:
         break;
-      case 3:
-        out_data["data_samples"][0]["current"] = js_data.val0;
-        logly_data["current"] = js_data.val0;
-        sendProtobuf(out_data);
-        client.log(logly_data);
-        sendJSON(logly_data);
+      case packetType.CURRENT:
+        JSON_data["current"] = js_data.val0;
+        client.log(JSON_data);
+        sendJSON(JSON_data);
         break;
-      case 4:
-        out_data["data_samples"][0]["clamp_temperature1"] = js_data.val0;
-        out_data["data_samples"][0]["clamp_temperature2"] = js_data.val1;
-        sendProtobuf(out_data);
-        logly_data["clamp_temperature1"] = js_data.val0;
-        logly_data["clamp_temperature2"] = js_data.val1;
-        client.log(logly_data);
-        sendJSON(logly_data);
+      case packetType.CLAMPS:
+        JSON_data["clamp_temperature1"] = js_data.val0;
+        JSON_data["clamp_temperature2"] = js_data.val1;
+        client.log(JSON_data);
+        sendJSON(JSON_data);
         break;
-      case 5:
-        out_data["data_samples"][0]["light"] = js_data.val0;
-        sendProtobuf(out_data);
-        logly_data["light"] = js_data.val0;
-        client.log(logly_data);
-        sendJSON(logly_data);
+      case packetType.LIGHT:
+        JSON_data["light"] = js_data.val0;
+        client.log(JSON_data);
+        sendJSON(JSON_data);
         break;
-      case 6:
-        out_data["data_samples"][0]["gas_pulse"] = js_data.val0;
-        logly_data["gas_pulse"] = js_data.val0;
-        sendProtobuf(out_data);
-        client.log(logly_data);
-        sendJSON(logly_data);
+      case packetType.GAS:
+        JSON_data["gas_pulse"] = js_data.val0;
+        client.log(JSON_data);
+        sendJSON(JSON_data);
         break;
     }
   });
 });
 
 serialPort.on("error", function (_error) {
-  log.err("Serial Port error: " + _error)
+  client.log("Serial Port error: " + _error)
 })
 
 
-/*
-              //log.info(value);
-              log.info(value.readInt16LE(6)/10.0);
-              log.info(value.readInt16LE(8)/10.0);
-              var data = {
-                devicekey: peripheral.uuid,
-                devicename: peripheral.advertisement.localName,
-                humidity: value.readInt16LE(8)/10.0,
-                temperature: value.readInt16LE(6)/10.0,
-    ext_temperature: value.readInt16LE(10)/10.0,
-    current: value.readInt16LE(12),
-    gas: value.readInt16LE(14)
-              }
-              client.post('graphs/', data, function (err, res, body) {
-                if(err) {
-                  log.info(err);
-                } else {
-                //log.info(err);
-                //log.info(res);
-                //log.info(body);
-                  return log.info(res.statusCode);
-                }
-              })
-            });
-            setTimeout(function(){
-              characteristic.notify(true, function(error){
-              log.info("notification enabled");
-              log.info(error);
-            }, 100);
-            })
-            log.info(characteristics[0].name);
-          });
-          //log.info(_service);
-        }
-      });
-    });
-  });
-  } catch (e) {
-    log.info(e);
-  }
-}
-
-*/
+process.on('uncaughtException', function(err) {
+    // handle the error safely
+    client.log('uncaughtException: ' + err);
+    var killtimer = setTimeout(function() {
+          process.exit(1);
+        }, 3000);
+    // But don't keep the process open just for that!
+    killtimer.unref();
+});
