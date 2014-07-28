dd16433f7d1b23b2813107fae92706f19bbcab06
Janek Mann <janekm@gmail.com>
2014-07-28 15:37:33 +0100

    alternative wiring build

--- deps.sh
+++ deps.sh
@@ -1,5 +1,8 @@
 apt-get update
 apt-get install libi2c-dev
-wget https://raw.github.com/hamishcunningham/wiringpi/master/package/2.13/unstable/wiringpi_2.13_armhf.deb
-dpkg -i ./wiringpi_2.13_armhf.deb
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+git pull origin
+./build
+gpio readall
 gpio load i2c
