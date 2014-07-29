556169780dbf92e0f44e54c7113876e6c0266d96
Janek Mann <janekm@gmail.com>
2014-07-29 13:33:10 +0100

    typo

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (config.piglow) {
 setInterval(function(){
   if (piglow) {
     piglow.startTransaction();
-    for (int i = 0; i < 16; i++) {
+    for (i = 0; i < 16; i++) {
       if (LEDs[i] > 0) {
         LEDs[i]--;
       }
