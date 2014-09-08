2ebfee8d91cac708a121287b6146e6c5b5be45dd
Janek Mann <janekm@gmail.com>
2014-09-08 17:49:08 +0100

    Implement second attempt at loading modules + restart in case of failure

--- startup.sh
+++ startup.sh
@@ -1,6 +1,13 @@
-sleep 10
+sleep 5
 modprobe i2c-dev
 modprobe i2c-bcm2708
+sleep 5
+modprobe i2c-dev
+modprobe i2c-bcm2708
+sleep 5
 cat /etc/modules
 sudo i2cdetect -y 1
+[ $? -ne 0 ]; then
+    sudo shutdown -r now
+fi
 node server.js
