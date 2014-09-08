029ddd74f70d284200ea17f6f5a1912b1b3836d8
Janek Mann <janekm@gmail.com>
2014-09-08 19:52:08 +0100

    list I2C device files

--- startup.sh
+++ startup.sh
@@ -1,10 +1,12 @@
+echo "" > /etc/modprobe.d/raspi-blacklist.conf
 sleep 5
-modprobe i2c-dev
-modprobe i2c-bcm2708
+sudo modprobe i2c-dev
+sudo modprobe i2c-bcm2708
 sleep 5
-modprobe i2c-dev
-modprobe i2c-bcm2708
+sudo modprobe i2c-dev
+sudo modprobe i2c-bcm2708
 sleep 5
+ls /dev/i2c*
 cat /etc/modules
 sudo i2cdetect -y 1
 if [ $? -ne 0 ]; then
