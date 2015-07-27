8169b68156dca0af1356dceea1b191b622b5c940
ngoddard <ngoddard@Nigels-MacBook-Air-2.local>
2015-07-27 11:10:36 +0100

    remove junk add i2c-tools .deb for faster install (needed for auto-restart) changed deps.sh to do faster auto-restart

--- deps.sh
+++ deps.sh
@@ -1,10 +1,16 @@
-sleep 40
-echo "apt-get updating..." >> deps.log
-apt-get update
-echo "apt-get removing i2ctools..." >> deps.log
-apt-get --assume-yes remove i2c-tools
-echo "apt-get installing i2ctools..." >> deps.log
-apt-get --assume-yes install i2c-tools
+echo " uninstalling i2c" > deps.log
+dpkg -r i2c-tools >> deps.log
+echo " installing i2c" >> deps.log
+dpkg -i i2c-tools_3.1.1-1_armhf.deb >> deps.log
+echo " deps.sh done" >> deps.log
+
+#sleep 40
+#echo "apt-get updating..." >> deps.log
+#apt-get update
+#echo "apt-get removing i2ctools..." >> deps.log
+#apt-get --assume-yes remove i2c-tools
+#echo "apt-get installing i2ctools..." >> deps.log
+#apt-get --assume-yes install i2c-tools
 #
 #cat /etc/modprobe.d/raspi-blacklist.conf
 #echo "" > /etc/modprobe.d/raspi-blacklist.conf
